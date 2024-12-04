2a7b33268db65872dceed4f1f0d8a33580ba75f5
HassanMunene <awanzihassan@gmail.com>
2024-12-04 17:49:15 +0300

    set up a vite proxy to connect to the backedn

--- frontend-mtambo/vite.config.js
+++ frontend-mtambo/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        // Django backend URL
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        // Strip the "/api" prefix
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [react()],
 })
