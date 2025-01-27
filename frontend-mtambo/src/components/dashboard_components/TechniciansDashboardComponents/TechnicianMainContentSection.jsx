4b8a1e549c366f5f833f3d6f4281d97aade04dbd
HassanMunene <awanzihassan@gmail.com>
2025-01-27 11:45:17 +0300

    checking if everything is okay

--- frontend-mtambo/src/components/dashboard_components/TechniciansDashboardComponents/TechnicianMainContentSection.jsx
+++ frontend-mtambo/src/components/dashboard_components/TechniciansDashboardComponents/TechnicianMainContentSection.jsx
@@ -5,7 +5,7 @@ const TechnicianMainContentSection = () => {
     return (
         <div className='pc-container'>
             <div className='pc-content'>
-                <div>YOO this is the technician dashboard</div>
+                <div>YOO this is the technician dashboard just an update</div>
             </div>
         </div>
     );
